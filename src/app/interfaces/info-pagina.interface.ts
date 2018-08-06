20b418f954c0fbdd06293dc1b60f8027394c502e
Ramon Barros <ramonfilipe@gmail.com>
2018-08-05 22:10:47 -0300

    final da parte 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,11 @@
+
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nome_curto?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  equipe_trabalho?: any[];
+}
